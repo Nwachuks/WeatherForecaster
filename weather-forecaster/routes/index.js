b59e7fcbdb807c634c28396508d1a5bb8340f646
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-05-12 17:16:28 +0100

    Add API parameters

--- weather-forecaster/routes/index.js
+++ weather-forecaster/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+let url = 'https://api.climacell.co/v3/weather/realtime';
+let appId = 'appid=0Y7tmnNKkocvtoOO74Km8v52IqEmdM5p';
+let units = '&units=metric';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
