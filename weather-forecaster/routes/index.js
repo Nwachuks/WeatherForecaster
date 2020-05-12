64096beeefde2d3eba4e4f1561a5c82bd2d1ab92
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-05-12 17:37:12 +0100

    Make API call

--- weather-forecaster/routes/index.js
+++ weather-forecaster/routes/index.js
@@ -8,7 +8,21 @@ let units = '&units=metric';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { body: '', forecast: ''});
+});
+
+router.post('/weather', function(req, res, next) {
+  let city = req.body.city;
+  url = url+city+"&"+appId;
+  request(url, function(error, response, body) {
+    body = JSON.parse(body);
+    if (error && response.statusCode != 200) {
+      throw error;
+    }
+    let country = (body.sys.country) ? body.sys.country : '';
+    let forecast = "For " + city + ", " + country;
+    res.render('index', { body: body, forecast: forecast });
+  });
 });
 
 module.exports = router;
